374b22ce9f6c0a2e862d00290085d4eaa142c7b0
Joffrey <origingod.oh@gmail.com>
2015-07-27 09:47:23 +0800

    updated

--- DataCrawer.py
+++ DataCrawer.py
@@ -2,36 +2,45 @@
 """
 Nothing to say
 """
+from __future__ import print_function
 import re
 import requests
 from BeautifulSoup import BeautifulSoup as soup
 from sqlalchemy.orm import sessionmaker
+import sys
 
 from Tasks import getContent
 import DataModel
 from DataModel import Salary
+import datetime
+import time
 
 LAGOU_JOB_URL = 'http://www.lagou.com/jobs/{0}.html'
 def getLagou(i,p={'job_description':('dd',{'class':'job_bt'}),
                   'job_request':('dd',{'class':'job_request'})}):
     return getContent(LAGOU_JOB_URL.format(i),patterns=p,ignore_pattern=['div',{'class':'position_del'}])
 
-DBSession = sessionmaker(DataModel.engine)
+#DBSession = sessionmaker(DataModel.engine)
 get_salary = r'(?P<low>[\d]{,3})k-(?P<high>[\d]{,3})k'
 salary_reg = re.compile(get_salary)
+
+SAVE_CSV = './save_file_' + str(datetime.datetime.fromtimestamp(time.time())) + '.csv'
 
 def saveLagou(job):
     res = re.match(salary_reg,job['job_request'])
     try:
-        session = DBSession()
-        res = res.groupdict()
-        salary = (int(res['low']) + int(res['high'])) / 2
-        jd = job['job_description']
-        s= Salary(salary_num=salary,job_description=jd)
-        session.add(s)
-        session.commit()
+        with open(SAVE_CSV,'a+') as f:
+            res = res.groupdict()
+            salary = (int(res['low']) + int(res['high'])) / 2
+            jd = job['job_description']
+            f.write('{0},{1}'.format(salary,jd.encode('utf8')))
     except Exception,e:
         print(e)
 
 if __name__=='__main__':
-    print(getLagou(20000))
+    for i in range(55300,999999):
+        try:
+            saveLagou(getLagou(i))
+            print('\r {0} id Finished\r'.format(i),file=sys.stdout)
+        except Exception,e:
+            pass
--- DataModel.py
+++ DataModel.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine,MetaData,Table
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 engine = create_engine('mysql://root@localhost/salary')
 
