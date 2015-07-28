51e94c33da198ef95d0899ec7c0e64a7650cbbad
Joffrey <origingod.oh@gmail.com>
2015-07-28 12:44:54 +0800

    anan

---
+++ Analyser.py
@@ -0,0 +1,22 @@
+#encoding: utf8
+"""
+Do some data-work
+"""
+
+
+from jieba.analyse import textrank
+
+def getWordwithWeight(sentence):
+    """
+    return
+    """
+    salary = float(sentence.split(',')[0])
+    jd = sentence[sentence.index(',')+1:]
+    ret = []
+    for (w,f) in textrank(jd,topK=50,
+                          withWeight=True,
+                          allowPOS=['n','eng','v','a','i','ns','vn']):
+        ret.append((w,f))
+    wordlist = [r[0] for r in ret]
+    flist = [r[1] for r in ret]
+    return ret,wordlist,flist
