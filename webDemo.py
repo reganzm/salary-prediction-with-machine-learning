f354b4acacedf4a8acafdb97d1881c45d36b889b
Joffrey <origingod.oh@gmail.com>
2015-08-06 18:11:02 +0800

    m

--- webDemo.py
+++ webDemo.py
@@ -1,25 +1,34 @@
-#encoding: utf8
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 
-from flask import Flask
-from flask import render_template
-from flask.ext.bootstrap import Bootstrap
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import requests
 
-
-from flask.ext.wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-class JdForm(Form):
-    name = StringField('输入你的jd', validators=[Required()])
-    submit = SubmitField('看看价格')
+import Analyser as A
+clf = A.get_clf()
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return render_template('templates/p.html',form=JdForm())
+def index():
+    return render_template('index.html')
+
+@app.route('/predict/',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        return render_template('predict.html',jd=request.form['jd'])
+    if request.method == 'GET':
+        return render_template('predict.html')
+
+@app.route('/do_predict/',methods=['POST'])
+def do_predict():
+    jd = request.form['jd']
+    X = A.get_single_X(jd)
+    salary = clf.predict(X)
+    return salary
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True)
