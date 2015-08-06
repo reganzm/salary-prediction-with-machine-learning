e6d05b6431e8de24526b25ada74d0ca41763497d
Joffrey <origingod.oh@gmail.com>
2015-08-06 15:25:31 +0800

    update webDemo.py add form

--- webDemo.py
+++ webDemo.py
@@ -1,14 +1,25 @@
 #encoding: utf8
 
 from flask import Flask
+from flask import render_template
 from flask.ext.bootstrap import Bootstrap
+
+
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class JdForm(Form):
+    name = StringField('输入你的jd', validators=[Required()])
+    submit = SubmitField('看看价格')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('templates/p.html',form=JdForm())
+
 
 if __name__ == '__main__':
     app.run(port=8000)
