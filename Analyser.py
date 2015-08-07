aa7fe64dc8fb04ff4c18a1fc7900abe3af328a5f
joffrey <origingod.oh@gmail.com>
2015-08-07 15:31:24 +0800

    update grid search

--- Analyser.py
+++ Analyser.py
@@ -19,9 +19,11 @@ from sklearn.cross_validation import train_test_split
 from sklearn.neural_network import BernoulliRBM
 from sklearn.pipeline import Pipeline
 from jieba.analyse import textrank
+from sklearn.grid_search import GridSearchCV
 
+Trained = False
 TopK_per_jd = 50
-TopK_Words = 2400
+TopK_Words = 4000
 
 def getWordwithWeightFromJD(jd):
     """
@@ -66,7 +68,7 @@ def genCountedDict():
         index = 0
         for l in f:
             try:
-                print('\r genCountedDict Processed {0} line of jd&salary. \r'.format(index),file=sys.stdout,end=" ")
+                print('\r genCountedDict Processed {0} line of jd&salary in {1}. \r'.format(index,sys.argv[1]),file=sys.stdout,end=" ")
                 index += 1
                 _,wl,fl,_ = getWordwithWeight(l)
                 if wl == None:
@@ -78,12 +80,18 @@ def genCountedDict():
         pickle.dump(count_dict,pf)
 
 def genWholeDict():
-    with open('CountedDict.pkl') as f:
-        cd = pickle.load(f)
-    scd = sorted(cd.items(),key=lambda k:k[1],reverse=True)
+    try:
+        with open('CountedDict.pkl') as f:
+            cd = pickle.load(f)
+        scd = sorted(cd.items(),key=lambda k:k[1],reverse=True)
+    except Exception,e:
+        print(e)
+        return None
     return scd
 
 def gen_func_single_XY():
+    if Trained == False:
+        return None,None
     whole_word_freq_list = genWholeDict()[:TopK_Words]
     whole_word_list = [wi[0] for wi in whole_word_freq_list]
     def single_XY(sentence):
@@ -112,11 +120,13 @@ def gen_func_single_XY():
 get_single_XY,get_single_X = gen_func_single_XY()
 
 def get_clf(clf_pickle="clf.pkl"):
+    if Trained == False:
+        return None
     with open(clf_pickle) as f:
         clf = pickle.load(f)
     return clf
 
-def genXY(fName,bSave=False,limit=None):
+def genXY(fName,bSave=False,limit=None,skip=0):
     """
     Ofcause return X,y
     it quite complex
@@ -130,10 +140,14 @@ def genXY(fName,bSave=False,limit=None):
     #whole_freq_list = [wi[1] for wi in whole_word_list]
     with open(fName) as f:
         index = 0
+        skip_index = skip
         for l in f:
             try:
-                print('\r GenXY Processed {0} line of jd&salary. \r'.format(index),file=sys.stdout,end=" ")
+                print('\r GenXY Processed {0} line of jd&salary in {1}. \r'.format(index,fName),file=sys.stdout,end=" ")
                 index += 1
+                if skip_index > 0:
+                    skip_index -= 1
+                    continue
                 _,wl,fl,salary = getWordwithWeight(l)
                 if wl == None:
                     continue
@@ -165,6 +179,7 @@ def Train():
     """
     Train Function
     """
+    print("loading data...")
     with open('X.pkl') as Xf:
         X = pickle.load(Xf)
     with open('Y.pkl') as Yf:
@@ -200,15 +215,22 @@ def Train():
         Y_test  = pickle.load(y_test_f)
     print("Load Data success!")
     """
-
+    print("Load Data success!")
+    params = {'rbm__learning_rate':0.07,
+              'rbm__n_iter':100,
+              'rbm__n_components':1100,
+              'logistic__C':4500}
     logistic = linear_model.LogisticRegression()
     rbm = BernoulliRBM(random_state=0, verbose=True)
-    rbm.learning_rate = 0.1
-    rbm.n_iter = 200
-    rbm.n_components = 1200
-    logistic.C = 6000.0
     clf = Pipeline(steps=[('rbm', rbm), ('logistic', logistic)])
-    clf.fit(X,Y)
+    #grid_search = GridSearchCV(clf,params,n_jobs = 1,verbose=1);
+    #print("start grid search")
+    #grid_search.fit(X[-len(Y)/400:],Y[-len(Y)/400:])
+    #print("Best score: %0.3f" % grid_search.best_score_)
+    #best_params = grid_search.best_score_
+    #clf.set_params(**best_params)
+    clf.set_params(**params)
+    clf.fit(X[-len(X)/2:],Y[-len(Y)/2:])
     #logistic_classifier = linear_model.LogisticRegression(C=100.0)
     #logistic_classifier.fit(X_train, Y_train)
     #print("Logistic regression using raw pixel features:\n%s\n" % (
@@ -222,7 +244,7 @@ def Train():
         clf.predict(X[-len(X)/4:]))))
     with open('clf.pkl','a+') as clf_f:
         pickle.dump(clf,clf_f)
-
+    #Trained = True
 
 if __name__ == '__main__':
     # generate a counted dictionary named 'CountDict.pkl'
@@ -235,5 +257,5 @@ if __name__ == '__main__':
     # 1. build an N-length array, generate X with this array
     # 2. paired with y
     if os.path.exists('X.pkl') == False:
-        genXY(sys.argv[1],bSave=True,limit=50000)
+        genXY(sys.argv[1],bSave=True,limit=50000,skip=20000)
     Train()
